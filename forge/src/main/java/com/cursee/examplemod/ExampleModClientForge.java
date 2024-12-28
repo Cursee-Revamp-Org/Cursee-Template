c8aa86d5d0d0172cb4cc41d121373401e86fc953
jason13official <jason13official@gmail.com>
2024-12-28 15:20:55 -0500

    fix forge client entrypoint

--- forge/src/main/java/com/cursee/examplemod/ExampleModClientForge.java
+++ forge/src/main/java/com/cursee/examplemod/ExampleModClientForge.java
@@ -1,6 +1,7 @@
 package com.cursee.examplemod;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
@@ -9,6 +10,7 @@ public class ExampleModClientForge {
     @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModBusEvents {
 
+        @SubscribeEvent
         public static void onClientSetup(final FMLClientSetupEvent event) {
             event.enqueueWork(() -> {
                 ExampleModClient.init();
@@ -16,6 +18,6 @@ public class ExampleModClientForge {
         }
     }
 
-    @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
-    public static class ForgeModBusEvents {}
+    // @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+    // public static class ForgeModBusEvents {}
 }
